c82240a66123cb8f02a5dd9948ae2133b3cb28db
Cory Hayes <hayescor@oregonstate.edu>
2018-02-12 18:14:18 -0800

    added some new unit tests to ApptTest

---
+++ projects/hayescor/assignment-3/Calendar/src/test/java/calendar/CalDayTest.java
@@ -0,0 +1,105 @@
+package calendar;
+/**
+ *  This class provides a basic set of test cases for the
+ *  CalDay class.
+ */
+import java.util.Calendar;
+import java.util.GregorianCalendar;
+
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class CalDayTest {
+	/**
+     * Test creating an instance of CalDay
+     */
+	 @Test
+	  public void test01()  throws Throwable  {
+		Calendar rightnow = Calendar.getInstance();
+    	int thisMonth = rightnow.get(Calendar.MONTH)+1;
+		int thisYear = rightnow.get(Calendar.YEAR);
+		int thisDay = rightnow.get(Calendar.DAY_OF_MONTH);
+		GregorianCalendar today = new GregorianCalendar(thisYear,thisMonth,thisDay);
+		//Construct new CalDay object
+		CalDay day = new CalDay(today); 
+	//assertions
+		assertTrue(day.isValid());
+		assertEquals(2, day.getMonth());
+		assertEquals(2018, day.getYear());
+		assertEquals(11, day.getDay());
+	 }
+	 /**
+     * Test setting and retrieving appts for a CalDay
+     */
+	 @Test
+	  public void test02()  throws Throwable  {
+		int startHour=21;
+		int startMinute=30;
+		int startDay=15;
+		int startMonth=01;
+		int startYear=2018;
+		String title="Test 2a";
+		String description="This is test 2a.";
+		 //Construct a new Appointment object with the initial data	 
+		Appt appt1 = new Appt(startHour,
+		          startMinute ,
+		          startDay ,
+		          startMonth ,
+		          startYear ,
+		          title,
+		         description);
+				 
+		startHour=8;
+		startMinute=15;
+		startDay=20;
+		startMonth=01;
+		startYear=2018;
+		title="Test 2b";
+		description="This is test 2b.";
+		 //Construct a new Appointment object with the initial data	 
+		Appt appt2 = new Appt(startHour,
+		          startMinute ,
+		          startDay ,
+		          startMonth ,
+		          startYear ,
+		          title,
+		         description);
+				 
+	// assertions
+		assertTrue(appt1.getValid());
+		assertTrue(appt2.getValid());
+		  
+		Calendar rightnow = Calendar.getInstance();
+    	int thisMonth = rightnow.get(Calendar.MONTH)+1;
+		int thisYear = rightnow.get(Calendar.YEAR);
+		int thisDay = rightnow.get(Calendar.DAY_OF_MONTH);
+		GregorianCalendar today = new GregorianCalendar(thisYear,thisMonth,thisDay);
+		//Construct new CalDay object
+		CalDay day = new CalDay(today);
+		day.addAppt(appt1);
+		day.addAppt(appt2);
+	//assertions
+		assertTrue(day.isValid());
+		assertEquals(2, day.getSizeAppts());                                   //check for two appts added	
+	 }
+	 /**
+     * Test string representation CalDay
+     */
+	 /*
+	 @Test
+	  public void test03()  throws Throwable  {
+		Calendar rightnow = Calendar.getInstance();
+    	int thisMonth = rightnow.get(Calendar.MONTH)+1;
+		int thisYear = rightnow.get(Calendar.YEAR);
+		int thisDay = rightnow.get(Calendar.DAY_OF_MONTH);
+		GregorianCalendar today = new GregorianCalendar(thisYear,thisMonth,thisDay);
+		//Construct new CalDay object
+		CalDay day = new CalDay(today); 
+	//assertions
+		assertTrue(day.isValid());
+		assertEquals("\t --- 2/1/2018 --- \n", day.toString());
+	 }
+	 */	
+}
---
+++ projects/hayescor/assignment-3/Calendar/src/test/java/calendar/TimeTableTest.java
@@ -0,0 +1,111 @@
+package calendar;
+/**
+ *  This class provides a basic set of test cases for the
+ *  TimeTable class.
+ */
+import java.util.Calendar;
+import java.util.GregorianCalendar;
+import java.util.LinkedList;
+import java.util.List;
+
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class TimeTableTest {
+	/** 
+	 * Create instance of CalDay and two appts, then test if TimeTable returns
+	 * proper list of CalDays
+	 */
+	 @Test
+	  public void test01()  throws Throwable  {
+		int startHour=21;
+		int startMinute=30;
+		int startDay=1;
+		int startMonth=2;
+		int startYear=2018;
+		String title="Test 2a";
+		String description="This is test 1a.";
+		 //Construct a new Appointment object with the initial data	 
+		Appt appt1 = new Appt(startHour,
+		          startMinute ,
+		          startDay ,
+		          startMonth ,
+		          startYear ,
+		          title,
+		         description);
+				 
+		startHour=8;
+		startMinute=15;
+		startDay=1;
+		startMonth=2;
+		startYear=2018;
+		title="Test 2b";
+		description="This is test 1b.";
+		 //Construct a new Appointment object with the initial data	 
+		Appt appt2 = new Appt(startHour,
+		          startMinute ,
+		          startDay ,
+		          startMonth ,
+		          startYear ,
+		          title,
+		         description);
+				 
+	// assertions
+		assertTrue(appt1.getValid());
+		assertTrue(appt2.getValid());
+		  
+		LinkedList<Appt> appts = new LinkedList<Appt>();
+		appts.add(appt1);
+		appts.add(appt2);
+		  
+		Calendar rightnow = Calendar.getInstance();
+    	int thisMonth = rightnow.get(Calendar.MONTH)+1;
+		int thisYear = rightnow.get(Calendar.YEAR);
+		int thisDay = rightnow.get(Calendar.DAY_OF_MONTH);
+		GregorianCalendar today = new GregorianCalendar(thisYear,thisMonth,thisDay);
+		GregorianCalendar tomorrow = (GregorianCalendar)today.clone();
+		tomorrow.add(Calendar.DAY_OF_MONTH,1);
+		//Construct new CalDay object
+		CalDay day = new CalDay(today);
+		day.addAppt(appt1);
+		day.addAppt(appt2);
+		TimeTable timeTable = new TimeTable();
+	//assertions
+		assertTrue(day.isValid());
+		assertEquals(2, day.getSizeAppts());                                      //check for two appts added
+		assertEquals(1, timeTable.getApptRange(appts, today, tomorrow).size());   //should return the CalDay between the two dates
+	 }
+	 /** 
+	 * Test for deletion of an appt
+	 */
+	 /*
+	 @Test
+	  public void test02()  throws Throwable  {
+		int startHour=21;
+		int startMinute=30;
+		int startDay=1;
+		int startMonth=2;
+		int startYear=2018;
+		String title="Test 2";
+		String description="This is test 2.";
+		 //Construct a new Appointment object with the initial data	 
+		Appt appt1 = new Appt(startHour,
+		          startMinute ,
+		          startDay ,
+		          startMonth ,
+		          startYear ,
+		          title,
+		         description);
+		
+		LinkedList<Appt> appts = new LinkedList<Appt>();
+		appts.add(appt1);
+		
+		TimeTable timeTable = new TimeTable();
+		timeTable.deleteAppt(appts, appt1);                           //delete target appt
+	//assertions
+		assertEquals(0, appts.size());                               //then check size of list
+	 }
+	 */
+}
